50d21452357887fa2efe9590355dcf191faf11ea
Andolino Baisac <dondonpentavia@gmail.com>
2026-02-19 00:50:01 +0800

    initial commit Spring boot rest api

---
+++ src/test/java/com/andolino/rest_api_service/RestApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andolino.rest_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
