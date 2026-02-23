182ab67f5f9ddfb0218f7cc63bde739a2db0e157
Andolino Baisac <dondonpentavia@gmail.com>
2026-02-23 19:50:42 +0800

    adding oowa

--- src/main/java/com/andolino/rest_api_service/controller/HomeController.java
+++ src/main/java/com/andolino/rest_api_service/controller/HomeController.java
@@ -1,13 +1,23 @@
 package com.andolino.rest_api_service.controller;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    private AtomicInteger totalRequests = new AtomicInteger(0);
+    private LocalDateTime lastRequestTime;
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Welcome to the REST API Service!";
+        int currentCount = totalRequests.incrementAndGet();
+        lastRequestTime = LocalDateTime.now();
+
+        return "Welcome oowa to the REST API Services!\n" +
+               "Total Requests: " + currentCount + "\n" +
+               "Last Request Time: " + lastRequestTime;
     }
 }
